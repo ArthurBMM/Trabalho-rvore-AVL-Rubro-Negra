8b5aec2fc14882de10d91b3cd4257d9c14a80247
Arthur Bandeira <arthurbmm76@gmail.com>
2026-05-21 12:16:40 -0300

    Feat: Rotação da direita simples implementada

--- ArovresBinarias/src/ArvoreAVL/Arvore_AVL.java
+++ ArovresBinarias/src/ArvoreAVL/Arvore_AVL.java
@@ -6,4 +6,12 @@ public class Arvore_AVL {
         int alturaDireita = no_avl.Obter_altura(no_avl.direita);
         no_avl.setAltura(1+ Math.max(alturaEsquerda, alturaDireita));
     }
+    public No_AVL Rotacao_Simples_Direita(No_AVL no_avl) {
+        No_AVL auxiliar = no_avl.esquerda;
+        no_avl.esquerda = no_avl.esquerda.direita;
+        auxiliar.direita = no_avl;
+        atualizarAltura(no_avl);
+        atualizarAltura(auxiliar);
+        return auxiliar;
+    }
 }
