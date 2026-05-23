4317e312ce4f2d8a1d2be740d1c53f79466676a5
Arthur Bandeira <arthurbmm76@gmail.com>
2026-05-23 16:08:25 -0300

    Feat: Método de inserção e Busca

--- ArovresBinarias/src/ArvoreAVL/Arvore_AVL.java
+++ ArovresBinarias/src/ArvoreAVL/Arvore_AVL.java
@@ -1,6 +1,7 @@
 package ArvoreAVL;
 
 public class Arvore_AVL {
+    private No_AVL raiz;
     public void atualizarAltura(No_AVL no_avl) {
         int alturaEsquerda = no_avl.Obter_altura(no_avl.getEsquerda());
         int alturaDireita = no_avl.Obter_altura(no_avl.getDireita());
@@ -29,5 +30,69 @@ public class Arvore_AVL {
     public No_AVL Rotacao_Dreita_Esquerda(No_AVL no_avl) {
         no_avl.setDireita(Rotacao_Simples_Direita(no_avl.getDireita()));
         return Rotacao_Simples_Esquerda(no_avl);
+    }
+    public No_AVL Inserir(No_AVL no_avl,int valor) {
+        if (no_avl == null) {
+            return new No_AVL(valor);
+        }
+        if (valor < no_avl.getValor()) {
+            no_avl.setEsquerda(Inserir(no_avl.getEsquerda(), valor));
+        }
+        if (valor > no_avl.getValor()) {
+            no_avl.setDireita(Inserir(no_avl.getDireita(), valor));
+        }
+        atualizarAltura(no_avl);
+        int fator_balanceamento = no_avl.Calcular_Fator_de_balanceamento(no_avl);
+        int fator_balnceamento_filho;
+        if (fator_balanceamento > 1) {
+            fator_balnceamento_filho = no_avl.Calcular_Fator_de_balanceamento(no_avl.getEsquerda());
+            if (fator_balnceamento_filho > 0) {
+                return Rotacao_Simples_Direita(no_avl);
+            }
+            if (fator_balnceamento_filho < 0) {
+                return Rotacao_Esquerda_Direita(no_avl);
+            }
+        }
+        if (fator_balanceamento < -1) {
+            fator_balnceamento_filho = no_avl.Calcular_Fator_de_balanceamento(no_avl.getDireita());
+            if (fator_balnceamento_filho < 0) {
+                return Rotacao_Simples_Esquerda(no_avl);
+            }
+            if (fator_balnceamento_filho > 0) {
+                return Rotacao_Dreita_Esquerda(no_avl);
+            }
+        }
+        return no_avl;
+    }
+    public No_AVL Buscar(No_AVL no_avl,int valor) {
+        if (no_avl == null) {
+            return null;
+        }
+        if (valor < no_avl.getValor()) {
+            return Buscar(no_avl.getEsquerda(), valor);
+        }
+        if (valor > no_avl.getValor()) {
+            return Buscar(no_avl.getDireita(), valor);
+        }
+        if (valor == no_avl.getValor()) {
+            return no_avl;
+        }
+        return null;
+    }
+
+    public No_AVL buscar(int valor) {
+        return Buscar(raiz, valor);
+    }
+
+    public No_AVL getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(No_AVL raiz) {
+        this.raiz = raiz;
+    }
+
+    public void inserir(int valor) {
+        raiz = Inserir(raiz,valor);
     }
 }
