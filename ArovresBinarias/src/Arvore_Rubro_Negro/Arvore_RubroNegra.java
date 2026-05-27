b6c61ea05134bd72cf76001b9ffeac81b51d54f6
Arthur Bandeira <arthurbmm76@gmail.com>
2026-05-27 08:52:33 -0300

    Feat: Criação das rotações a esquerda e direita

--- ArovresBinarias/src/Arvore_Rubro_Negro/Arvore_RubroNegra.java
+++ ArovresBinarias/src/Arvore_Rubro_Negro/Arvore_RubroNegra.java
@@ -9,4 +9,43 @@ public class Arvore_RubroNegra {
         nil.cor = No_RubroNegro.PRETO;
         raiz = nil;
     }
+
+    // ---------- ROTAÇÕES ----------
+    private void rotacaoEsquerda(No_RubroNegro x) {
+        No_RubroNegro y = x.direita;
+        x.direita = y.esquerda;
+        if (y.esquerda != nil) {
+            y.esquerda.pai = x;
+        }
+        y.pai = x.pai;
+        if (x.pai == nil) {
+            raiz = y;
+        } else if (x == x.pai.esquerda) {
+            x.pai.esquerda = y;
+        } else {
+            x.pai.direita = y;
+        }
+        y.esquerda = x;
+        x.pai = y;
+    }
+    private void rotacaoDireita(No_RubroNegro x) { // espelho da esquerda
+        No_RubroNegro y = x.esquerda;
+        x.esquerda = y.direita;
+        if (y.direita != nil) {
+            y.direita.pai = x;
+        }
+        y.pai = x.pai;
+        if (x.pai == nil) {
+            raiz = y;
+        } else if (x == x.pai.direita) {
+            x.pai.direita = y;
+        } else {
+            x.pai.esquerda = y;
+        }
+        y.direita = x;
+        x.pai = y;
+    }
+
+
+
 }
