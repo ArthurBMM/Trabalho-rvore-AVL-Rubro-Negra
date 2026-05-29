16df74ffa31603cd002b801abdf01e71cb087d8d
Amanda <amandaribeiro9255@gmail.com>
2026-05-29 02:23:42 -0300

    "fix: adiciona Main como ponto de entrada principal chamando StressTest - conforme solicitado pelo QA"

--- ArovresBinarias/src/Testes/Main.java
+++ ArovresBinarias/src/Testes/Main.java
@@ -4,6 +4,6 @@ package Testes;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Testes.StressTest.main(args);
     }
 }
